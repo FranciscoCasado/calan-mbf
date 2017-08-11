24b2df7f8f1b402bf996fbbb06cab8458c7db349
Francisco Casado <panchoc93@hotmail.com>
2017-08-11 09:42:48 -0400

    .

--- models/adc16_chan_power/adc16_plot_snap.py
+++ models/adc16_chan_power/adc16_plot_snap.py
@@ -7,40 +7,41 @@ import time
 import pylab
 import numpy
 
+
 def get_snap(snap_name):
-	raw_data = numpy.fromstring(fpga.snapshot_get(snap_name, man_trig=True, man_valid=True)['data'], dtype='<i1')
-	chan_1 = []
-	chan_2 = []
-	chan_3 = []
-	chan_4 = []
-	# interleave
-	for i in xrange(0,65536):
-		chan_1.append(raw_data[i*4])
-		chan_2.append(raw_data[i*4+1])
-		chan_3.append(raw_data[i*4+2])
-		chan_4.append(raw_data[i*4+3])
-	return [chan_1, chan_2, chan_3, chan_4]
+    raw_data = numpy.fromstring(fpga.snapshot_get(snap_name, man_trig=True, man_valid=True)['data'], dtype='<i1')
+    chan_1 = []
+    chan_2 = []
+    chan_3 = []
+    chan_4 = []
+    # interleave
+    for i in xrange(0,65536):
+        chan_1.append(raw_data[i*4])
+        chan_2.append(raw_data[i*4+1])
+        chan_3.append(raw_data[i*4+2])
+        chan_4.append(raw_data[i*4+3])
+    return [chan_1, chan_2, chan_3, chan_4]
+
 
 def plot_snap():
-	matplotlib.pyplot.clf()
-	snap_num = 2
-	snap_name = ['snap_a','snap_b','snap_c','snap_d']
+    matplotlib.pyplot.clf()
+    snap_num = 2
+    snap_name = ['snap_out_a','snap_out_b','snap_out_c','snap_out_d']
 
-	for i in range(0,4):
-		matplotlib.pylab.s
-		data = get_snap(snap_name[i])
-		matplotlib.pylab.plot(data[0])
-		matplotlib.pylab.plot(data[1])
-		matplotlib.pylab.plot(data[2])
-		matplotlib.pylab.plot(data[3])
-	# matplotlib.pylab.semilogy(interleave_a)
-	matplotlib.pylab.ylabel('Sample value ')
-	#matplotlib.pylab.ylim(0)
-	matplotlib.pylab.grid()
-	matplotlib.pylab.xlabel('Sample Number')
-	matplotlib.pylab.xlim(0, 1024)
-	fig.canvas.draw()
-	#fig.canvas.manager.window.after(500, plot_snap)
+    for i in range(0,4):
+        data = get_snap(snap_name[i])
+        matplotlib.pylab.plot(data[0])
+        matplotlib.pylab.plot(data[1])
+        matplotlib.pylab.plot(data[2])
+        matplotlib.pylab.plot(data[3])
+    # matplotlib.pylab.semilogy(interleave_a)
+    matplotlib.pylab.ylabel('Sample value ')
+    # matplotlib.pylab.ylim(0)
+    matplotlib.pylab.grid()
+    matplotlib.pylab.xlabel('Sample Number')
+    matplotlib.pylab.xlim(0, 1024)
+    fig.canvas.draw()
+    # fig.canvas.manager.window.after(500, plot_snap)
 
 fpga = corr.katcp_wrapper.FpgaClient('192.168.1.13')
 
