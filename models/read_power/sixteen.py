880552db871371b2a735ff273462aab3b5b37d02
Francisco Casado <panchoc93@hotmail.com>
2017-10-31 14:41:21 -0300

    added little features and working on calibration_defaults

--- models/read_power/sixteen.py
+++ models/read_power/sixteen.py
@@ -10,13 +10,14 @@ import pylab
 import numpy as np
 import random
 
+
 class LiveChannels(animation.TimedAnimation):
     def __init__(self, fpga, fig):
         self.channels = [None]*16
         self.letters = ['a', 'b', 'c', 'd']
         self.fpga = fpga
 
-        self.t = np.linspace(0, 1023, 1024) #needed as x domain
+        self.t = np.linspace(0, 1023, 1024)  # needed as x domain
 
         self.fig = fig
         self.axes = [None]*16
@@ -25,12 +26,12 @@ class LiveChannels(animation.TimedAnimation):
             self.axes[i] = self.fig.add_subplot(4, 4, i+1)
             # axes[i].set_xlabel('N')
             # axes[i].set_ylabel(self.letters[i/4]+str(i%4+1))
-            self.axes[i].set_title(self.letters[i/4]+str(i%4+1))
+            self.axes[i].set_title(self.letters[i/4]+str(i % 4+1))
             self.lines[i] = Line2D([], [], color='blue')
             self.axes[i].add_line(self.lines[i])
             self.axes[i].set_xlim(0, 64)
             self.axes[i].set_ylim(-128, 128)
-            #axes[i].set_aspect('equal', 'datalim')
+            # axes[i].set_aspect('equal', 'datalim')
 
         plt.tight_layout()  # prevent text & graphs overlapping
 
@@ -53,10 +54,9 @@ class LiveChannels(animation.TimedAnimation):
 
     def read_snap(self, snap_name):
         raw_data = np.fromstring(self.fpga.snapshot_get(snap_name, man_trig=True, man_valid=True)['data'], dtype='<i1')
-        # generate fake data
-        #fake_t = np.linspace(0, 1023, 1024*4)
-        #raw_data = np.sin(2 * np.pi * (fake_t + random.randint(-3, 3)) / 10.) * (100 + random.randint(-10, 10))
-
+        # Generate fake data
+        # fake_t = np.linspace(0, 1023, 1024*4)
+        # raw_data = np.sin(2 * np.pi * (fake_t + random.randint(-3, 3)) / 10.) * (100 + random.randint(-10, 10))
 
         sub_chan_1 = []
         sub_chan_2 = []
@@ -75,13 +75,10 @@ class LiveChannels(animation.TimedAnimation):
         for i in range(4):
             data = self.read_snap('snap_'+self.letters[i])
             self.channels[i*4+0] = data[0]
-            #print(i*4+0)
             self.channels[i*4+1] = data[1]
-            #print(i*4+1)
             self.channels[i*4+2] = data[2]
-            #print(i*4+2)
             self.channels[i*4+3] = data[3]
-            #print(i*4+3)
+
 
 class Powers(animation.TimedAnimation):
     def __init__(self, fpga, fig):
@@ -89,10 +86,19 @@ class Powers(animation.TimedAnimation):
         self.letters = ['a', 'b', 'c', 'd']
         self.fpga = fpga
 
-        self.t = np.linspace(1, 16, 16) # needed as x domain
+        self.t = np.linspace(1, 16, 16)  # needed as x domain
+        self.rms_mean_dB = np.zeros(16)
+        self.rms_dev_dB = np.zeros(16)
 
         self.fig = fig
         self.axes = fig.add_subplot(1, 1, 1)
+        self.bars = self.axes.bar(self.t, self.powers)
+        self.line_mean = Line2D([], [], color='black')
+        self.axes.add_line(self.line_mean)
+        self.line_dev_sup = Line2D([], [], color='black', linestyle='dashed')
+        self.axes.add_line(self.line_dev_sup)
+        self.line_dev_inf = Line2D([], [], color='black', linestyle='dashed')
+        self.axes.add_line(self.line_dev_inf)
         self.axes.set_title('Powers')
         self.axes.set_xlim(1, 16)
         self.axes.set_ylim(-20, 5)
@@ -105,45 +111,46 @@ class Powers(animation.TimedAnimation):
 
     def _draw_frame(self, framedata):
         self.update_data()
-        # self.line.set_data(self.t, np.array(self.powers))
-        self.axes.clear()
-        self.axes.set_ylim(-20, 5)
-        self.axes.grid('on')
-        self.axes.bar(self.t, self.powers)
-        # self._drawn_artists = [self.line]
+        # self.axes.clear()
+        for i in range(16):
+            self.bars[i].set_height(self.powers[i])
+        # self.axes.set_ylim(-20, 5)
+        # self.axes.grid('on')
+        self.line_mean.set_data(self.t, self.rms_mean_dB)
+        self.line_dev_sup.set_data(self.t, self.rms_mean_dB + self.rms_dev_dB)
+        self.line_dev_inf.set_data(self.t, self.rms_mean_dB - self.rms_dev_dB)
+        # self._drawn_artists = [self.line_mean, self.line_dev_sup, self.line_dev_inf]
 
     def new_frame_seq(self):
         return iter(range(1))
 
     def _init_draw(self):
-        self.axes.bar(self.t, self.powers)
+        lines = [self.line_mean, self.line_dev_sup, self.line_dev_inf]
+        for l in lines:
+            l.set_data([], [])
 
     def read_regs(self):
         self.fpga.write_int('hold_data', 1)
         self.fpga.write_int('hold_data', 0)
-        rms_a1 = self.fpga.read_uint('reg_a1')/2.0**15
-        rms_a2 = self.fpga.read_uint('reg_a2')/2.0**15
-        rms_a3 = self.fpga.read_uint('reg_a3')/2.0**15
-        rms_a4 = self.fpga.read_uint('reg_a4')/2.0**15
-        rms_b1 = self.fpga.read_uint('reg_b1')/2.0**15
-        rms_b2 = self.fpga.read_uint('reg_b2')/2.0**15
-        rms_b3 = self.fpga.read_uint('reg_b3')/2.0**15
-        rms_b4 = self.fpga.read_uint('reg_b4')/2.0**15
-        rms_c1 = self.fpga.read_uint('reg_c1')/2.0**15
-        rms_c2 = self.fpga.read_uint('reg_c2')/2.0**15
-        rms_c3 = self.fpga.read_uint('reg_c3')/2.0**15
-        rms_c4 = self.fpga.read_uint('reg_c4')/2.0**15
-        rms_d1 = self.fpga.read_uint('reg_d1')/2.0**15
-        rms_d2 = self.fpga.read_uint('reg_d2')/2.0**15
-        rms_d3 = self.fpga.read_uint('reg_d3')/2.0**15
-        rms_d4 = self.fpga.read_uint('reg_d4')/2.0**15
-
-        return [rms_a1, rms_a2, rms_a3, rms_a4,
-               rms_b1, rms_b2, rms_b3, rms_b4,
-               rms_c1, rms_c2, rms_c3, rms_c4,
-               rms_d1, rms_d2, rms_d3, rms_d4]
+        rms = np.array([self.fpga.read_uint('reg_a1'),
+                        self.fpga.read_uint('reg_a2'),
+                        self.fpga.read_uint('reg_a3'),
+                        self.fpga.read_uint('reg_a4'),
+                        self.fpga.read_uint('reg_b1'),
+                        self.fpga.read_uint('reg_b2'),
+                        self.fpga.read_uint('reg_b3'),
+                        self.fpga.read_uint('reg_b4'),
+                        self.fpga.read_uint('reg_c1'),
+                        self.fpga.read_uint('reg_c2'),
+                        self.fpga.read_uint('reg_c3'),
+                        self.fpga.read_uint('reg_c4'),
+                        self.fpga.read_uint('reg_d1'),
+                        self.fpga.read_uint('reg_d2'),
+                        self.fpga.read_uint('reg_d3'),
+                        self.fpga.read_uint('reg_d4')])/(2.0**15)
+        return rms
 
     def update_data(self):
-        #print("******")
-        self.powers = np.log10(np.array(self.read_regs()))*10
-        #print(self.powers)
+        self.powers = np.log10(self.read_regs())*10
+        self.rms_mean_dB = np.zeros(16) + np.mean(self.powers)
+        self.rms_dev_dB = np.zeros(16)  + np.std(self.powers)
--- utils/down_converters/mixer.py
+++ utils/down_converters/mixer.py
@@ -2,131 +2,162 @@ import spidev
 import RPi.GPIO as GPIO
 
 
-WRITE = 0
-READ = 1
+_WRITE = 0
+_READ = 1
 
-default =   [[0, 0, 0x022], [0, 1, 0x0ff], [0, 2, 0x1a0], [0, 3, 0x000], [0, 4, 0x31c],
-			[0, 5, 0x000], [0, 6, 0x3ff], [0, 7, 0x024], [0, 8, 0x000], [0, 9, 0x00f],
-			[0, 10, 0x000], [0, 11, 0x060], [0, 13, 0x000], [0, 14, 0x360], [0, 15, 0x242],
-			[0, 16, 0x380], [0, 17, 0x000], [0, 18, 0x080], [0, 19, 0x05f], [0, 20, 0x1ea],
-			[0, 21, 0x0bf], [0, 22, 0x1b8], [0, 23, 0x065], [0, 24, 0x24f], [0, 25, 0x3a8],
-			[0, 26, 0x015], [0, 27, 0x180], [0, 28, 0x063], [0, 29, 0x000], [0, 30, 0x000],
-			[0, 31, 0x000], [1, 1, 0x000], [1, 2, 0x000], [1, 3, 0x000], [1, 4, 0x380],
-			[1, 5, 0x000], [1, 6, 0x000], [1, 7, 0x000], [1, 8, 0x1aa], [1, 9, 0x114],
-			[1, 10, 0x354], [1, 11, 0x073], [1, 12, 0x000], [1, 13, 0x000], [1, 14, 0x000],
-			[1, 15, 0x000], [1, 16, 0x000], [1, 17, 0x000], [1, 18, 0x000], [1, 19, 0x000],
-			[1, 20, 0x000], [1, 21, 0x000], [1, 22, 0x000], [1, 23, 0x000], [1, 24, 0x0c4],
-			[1, 25, 0x12b], [1, 26, 0x165], [1, 27, 0x000], [1, 28, 0x004], [1, 31, 0x000]]
+_default = [[0, 0, 0x022], [0, 1, 0x0ff], [0, 2, 0x1a0], [0, 3, 0x000], [0, 4, 0x31c],
+            [0, 5, 0x000], [0, 6, 0x3ff], [0, 7, 0x024], [0, 8, 0x000], [0, 9, 0x00f],
+            [0, 10, 0x000], [0, 11, 0x060], [0, 13, 0x000], [0, 14, 0x360], [0, 15, 0x242],
+            [0, 16, 0x380], [0, 17, 0x000], [0, 18, 0x080], [0, 19, 0x05f], [0, 20, 0x1ea],
+            [0, 21, 0x0bf], [0, 22, 0x1b8], [0, 23, 0x065], [0, 24, 0x24f], [0, 25, 0x3a8],
+            [0, 26, 0x015], [0, 27, 0x180], [0, 28, 0x063], [0, 29, 0x000], [0, 30, 0x000],
+            [0, 31, 0x000], [1, 1, 0x000], [1, 2, 0x000], [1, 3, 0x000], [1, 4, 0x380],
+            [1, 5, 0x000], [1, 6, 0x000], [1, 7, 0x000], [1, 8, 0x1aa], [1, 9, 0x114],
+            [1, 10, 0x354], [1, 11, 0x073], [1, 12, 0x000], [1, 13, 0x000], [1, 14, 0x000],
+            [1, 15, 0x000], [1, 16, 0x000], [1, 17, 0x000], [1, 18, 0x000], [1, 19, 0x000],
+            [1, 20, 0x000], [1, 21, 0x000], [1, 22, 0x000], [1, 23, 0x000], [1, 24, 0x0c4],
+            [1, 25, 0x12b], [1, 26, 0x165], [1, 27, 0x000], [1, 28, 0x004], [1, 31, 0x000]]
 
+_boards = {'a': 0, 'b': 1, 'c': 2, 'd': 3, }
+_ports = {'1': 1, '2': 2, '3': 4, '4': 5, }
 
-setLocalAddrs = [0, 0, 0x023];
-standbyMode = [0, 0, 0x026];
-rxMode = [0, 0, 0x02A];
-doutEn = [0, 14,0x362];
+_set_local_addrs = [0, 0, 0x023]
+_standbyMode = [0, 0, 0x026]
+_rxMode = [0, 0, 0x02A]
+_doutEn = [0, 14, 0x362]
 
-A1 = 15
-A2 = 13
-
-
-def bitmask(n):
-	return int('1'*n, 2)
-
-def bitread(n,i):
-	return int("{0:02b}".format(n)[-(i+1)])	
-
-def mensaje(rw, a, d):
-	return ((rw<<15) | ((a & bitmask(5))<<10) | (d & bitmask(10)))
+_A1 = 15
+_A2 = 13
 
 
+def _bitmask(n):
+    return int('1'*n, 2)
 
-class Mixer():
-	"""docstring for Mixer"""
-	def __init__(self, spidev):
-		global WRITE, READ, default, setLocalAddrs,standbyMode, rxMode, doutEn, A1, A2
 
-		# GPIO config
-		GPIO.setmode(GPIO.BOARD)
-		GPIO.setup(A1, GPIO.OUT, initial=GPIO.LOW)
-		GPIO.setup(A2, GPIO.OUT, initial=GPIO.LOW)
+def _bitread(n, i):
+    return int("{0:02b}".format(n)[-(i+1)])
 
-		#SPI config
-		bus = 0
-		device = 0
-		self.spi = spidev
-		self.spi.open(bus, device)
-		self.spi.max_speed_hz = 1000000
 
-		## LO parameters
-		self.N = 0b01001000
-		self.F = 0b10000000000000000000
-		self.Flow = self.F & bitmask(10);
-		self.Fhigh = (self.F >> 10) & bitmask(10);
+def _message(rw, a, d):
+    return (rw << 15) | ((a & _bitmask(5)) << 10) | (d & _bitmask(10))
 
-	## Board actions
-	def init_all(self):
-		self.init_board(0)
-		self.init_board(1)
-		self.init_board(2)
-		self.init_board(3)
 
-	def init_board(self, board):
-		self.select_board(board)
-		for i in xrange(len(default)):
-			if not default[i][0]:
-				self.write_reg(default[i])
-				if default[i+1][0]:
-					self.write_reg(setLocalAddrs)
-				else:
-					self.write_reg(default[i])
-		self.write_reg(standbyMode)
-		self.write_reg(doutEn)
-		self.write_reg(15, (self.get_default(15) & ~bitmask(7)) | self.N);
-		self.write_reg(16, self.Fhigh);
-		self.write_reg(17, self.Flow);
-		self.write_reg(5,1)
-		self.write_reg(rxMode)
-		self.write_reg(5,0)
-		print('Done configuring '+str(board))
+class Mixer:
+    """docstring for Mixer"""
+    def __init__(self):
+        global _WRITE, _READ, _default, _set_local_addrs, _A1, _A2
+        global _standbyMode, _rxMode, _doutEn, _ports, _boards
 
-	def select_board(self,board):
-		assert(board<4)
-		GPIO.output(A1, bitread(board,0))
-		GPIO.output(A2, bitread(board,1))
+        # GPIO config
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(_A1, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(_A2, GPIO.OUT, initial=GPIO.LOW)
 
-	def standby(self, board):
-		if board == 'all':
-			for i in range(4):
-				select_board(i)
-				self.write_reg(standbyMode)
-		else:
-			select_board(board)
-			self.write_reg(standbyMode)
+        # SPI config
+        bus = 0
+        device = 0
+        self.spi = spidev.SpiDev()
+        self.spi.open(bus, device)
+        self.spi.max_speed_hz = 1000000
 
-	def set_gain(self, board, channel, value=255):
-		self.select_board(board)
-		self.write_reg(6, 1<<(channel+4) | (self.get_default(6) & bitmask(5)))
-		self.write_reg(1, value | (self.get_default(1) & ~bitmask(8)))
+        # LO parameters
+        self._N = 0b01001000
+        self._F = 0b10000000000000000000
+        self._Flow = self._F & _bitmask(10)
+        self._Fhigh = (self._F >> 10) & _bitmask(10)
 
-	#SPI communication
-	def write_reg(self, addrs, data = None):
-		if data == None:
-			self.write_reg(addrs[1],addrs[2])
-			return
-		m = mensaje(WRITE,addrs,data)
-		send = [m >> 8, m & bitmask(8)]
-		self.spi.xfer2(send)
+    # Board actions
+    def init_all(self):
+        self.init_board(0)
+        self.init_board(1)
+        self.init_board(2)
+        self.init_board(3)
 
-	def read_reg(self, addrs):
-		m = mensaje(READ,addrs,0)
-		send = [m >> 8, m & bitmask(8)]
-		read = self.spi.xfer2(send)
-		return (read[0]<<8) | (read[1] & bitmask(8))
+    def init_board(self, board):
+        self._select_board(board)
+        for i in xrange(len(_default)):
+            if not _default[i][0]:
+                self.write_reg(_default[i])
+                if _default[i+1][0]:
+                    self.write_reg(_set_local_addrs)
+                else:
+                    self.write_reg(_default[i])
+        self.write_reg(_standbyMode)
+        self.write_reg(_doutEn)
+        self.write_reg(15, (self.get_default(15) & ~_bitmask(7)) | self._N)
+        self.write_reg(16, self._Fhigh)
+        self.write_reg(17, self._Flow)
+        self.write_reg(5, 1)
+        self.write_reg(_rxMode)
+        self.write_reg(5, 0)
+        print('Done configuring '+str(board))
 
-	def get_default(self, addrs):
-		for i in default:
-			if i[1] == addrs:
-				return i[2]
-		return -1
+    def calibrate_all(self):
+    	self.set_gain('a1',7,15)
+    	self.set_gain('a2',7,15)
+    	self.set_gain('a3',7,15)
+    	self.set_gain('a4',7,15)
+    	self.set_gain('b1',7,15)
+    	self.set_gain('b2',7,15)
+    	self.set_gain('b3',7,15)
+    	self.set_gain('b4',7,15)
+    	self.set_gain('c1',7,15)
+    	self.set_gain('c2',7,15)
+    	self.set_gain('c3',7,15)
+    	self.set_gain('c4',7,15)
+    	self.set_gain('d1',7,15)
+    	self.set_gain('d2',7,15)
+    	self.set_gain('d3',7,15)
+    	self.set_gain('d4',7,15)
 
-	def cleanGPIO(self):
-		GPIO.cleanup()
+    def _select_board(self,board):
+        assert(board < 4)
+        GPIO.output(_A1, _bitread(board, 0))
+        GPIO.output(_A2, _bitread(board, 1))
+
+    def standby(self, board):
+        if board == 'all':
+            for i in range(4):
+                self._select_board(i)
+                self.write_reg(_standbyMode)
+        else:
+            self._select_board(board)
+            self.write_reg(_standbyMode)
+
+    def set_gain(self, channel, lna_gain=7, vga_gain=15):
+        if len(channel) > 2:
+            print("invalid channel")
+            return
+        try:
+            board = _boards[channel[0]]
+            port = _ports[channel[1]]
+        except KeyError:
+            print("invalid channel")
+            return
+        self._select_board(board)
+        self.write_reg(6, (self.get_default(6) & _bitmask(5)) | 1 << (port+4))
+        self.write_reg(1, (self.get_default(1) & ~_bitmask(8)) | lna_gain << 5 | vga_gain)
+
+    # SPI communication
+    def write_reg(self, addrs, data=None):
+        if data is None:
+            self.write_reg(addrs[1], addrs[2])
+            return
+        m = _message(_WRITE, addrs, data)
+        send = [m >> 8, m & _bitmask(8)]
+        self.spi.xfer2(send)
+
+    def read_reg(self, addrs):
+        m = _message(_READ, addrs, 0)
+        send = [m >> 8, m & _bitmask(8)]
+        read = self.spi.xfer2(send)
+        return (read[0] << 8) | (read[1] & _bitmask(8))
+
+    def get_default(self, addrs):
+        for i in _default:
+            if i[1] == addrs:
+                return i[2]
+        return -1
+
+    def clean_gpio(self):
+        GPIO.cleanup()
